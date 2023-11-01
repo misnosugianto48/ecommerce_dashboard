16849a6f1f26a18d621d4d9b4a3a8aad96ccfc16
misnosugianto48 <misnosugianto48@gmail.com>
2023-11-01 18:35:21 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Eccomerce Public Dashboard
+
+## Setup Environment
+
+conda activate main-ds
+pip install streamlit babel numpy pandas matplotlib seaborn
+
+## Dataset
+
+<https://drive.google.com/drive/folders/15-pJ70W66vrPTjMwbHyVWPuGIwW4D0Hn?usp=sharing>
+
+## Run Streamlit App
+
+streamlit run dashboard.py
---
+++ dashboard/dashboard.py
@@ -0,0 +1,262 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+
+def create_daily_orders_df(df):
+    daily_orders_df = df.resample(rule='D', on='order_approved_at').agg({
+        "order_id": "nunique",
+        "price": "sum"
+    })
+    daily_orders_df = daily_orders_df.reset_index()
+    daily_orders_df.rename(columns={
+        "order_id": "order_count",
+        "price": "revenue"
+    }, inplace=True)
+
+    return daily_orders_df
+
+
+def create_sum_order_items_df(df):
+    sum_order_items_df = df.groupby("product_category_name_english").review_score.sum(
+    ).sort_values(ascending=False).reset_index()
+    return sum_order_items_df
+
+
+def create_bystate_df(df):
+    bystate_df = df.groupby(
+        by="customer_state").customer_id.nunique().reset_index()
+    bystate_df.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
+    return bystate_df
+
+
+def create_bypayment_df(df):
+    bypayment_df = df.groupby(by="payment_type").customer_id.nunique(
+    ).sort_values(ascending=False).reset_index()
+    bypayment_df.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
+    return bypayment_df
+
+
+def create_rfm_df(df):
+    rfm_df = df.groupby(by="customer_id", as_index=False).agg({
+        "order_approved_at": "max",
+        "order_id": "nunique",
+        "price": "sum"
+    })
+    rfm_df.columns = ["customer_id",
+                      "max_order_timestamp", "frequency", "monetary"]
+
+    rfm_df["max_order_timestamp"] = rfm_df["max_order_timestamp"].dt.date
+    recent_date = df["order_approved_at"].dt.date.max()
+    rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(
+        lambda x: (recent_date - x).days)
+    rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
+
+    return rfm_df
+
+
+# load data
+all_df = pd.read_csv("../data/main_data.csv", low_memory=False, dtype={
+                     "product_length_cm": "object", "product_height_cm": "object", "product_width_cm": "object"})
+
+# pastikan datetime series
+datetime_columns = ["order_purchase_timestamp",
+                    "order_approved_at",
+                    "order_delivered_carrier_date",
+                    "order_delivered_customer_date",
+                    "order_estimated_delivery_date"]
+all_df.sort_values(by="order_approved_at", inplace=True)
+all_df.reset_index(inplace=True)
+
+for column in datetime_columns:
+    all_df[column] = pd.to_datetime(all_df[column])
+
+# fiiltering data
+min_date = all_df["order_approved_at"].min()
+max_date = all_df["order_approved_at"].max()
+
+st.header('Ecommerce Public Dashboard :sparkles:')
+
+with st.sidebar:
+    st.image("https://plus.unsplash.com/premium_photo-1675878786222-e578e7817527?auto=format&fit=crop&q=80&w=928&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", width=100)
+    st.title("Eccomerce Public")
+
+    # mengambil start dan end date dari date input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu', min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+
+main_df = all_df[(all_df["order_approved_at"] >= str(start_date)) &
+                 (all_df["order_approved_at"] <= str(end_date))]
+
+# menyiapkan data frame
+daily_orders_df = create_daily_orders_df(main_df)
+sum_order_items_df = create_sum_order_items_df(main_df)
+bypayment_df = create_bypayment_df(main_df)
+bystate_df = create_bystate_df(main_df)
+rfm_df = create_rfm_df(main_df)
+
+# orders
+st.subheader('Daily Orders')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    total_orders = daily_orders_df.order_count.sum()
+    st.metric("Total orders", value=total_orders)
+
+with col2:
+    total_revenue = format_currency(
+        daily_orders_df.revenue.sum(), "AUD", locale='es_CO')
+    st.metric("Total Revenue", value=total_revenue)
+
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    daily_orders_df["order_approved_at"],
+    daily_orders_df["order_count"],
+    marker='o',
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+
+st.pyplot(fig)
+
+# Product Review
+st.subheader("Best & Worst Reviewing Product Category")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(x="review_score", y="product_category_name_english",
+            data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_title("Best Reviewing Product", loc="center", fontsize=50)
+ax[0].tick_params(axis='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=30)
+
+sns.barplot(x="review_score", y="product_category_name_english", data=sum_order_items_df.sort_values(
+    by="review_score", ascending=True).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Worst Reviewing Product", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+
+st.pyplot(fig)
+
+st.subheader("Customer Demographics")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    fig, ax = plt.subplots(figsize=(20, 10))
+
+    colors = ["#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+    sns.barplot(
+        y="customer_count",
+        x="payment_type",
+        data=bypayment_df.sort_values(by="customer_count", ascending=False),
+        palette=colors,
+        ax=ax
+    )
+    ax.set_title("Number of Customer by Payment", loc="center", fontsize=50)
+    ax.set_ylabel(None)
+    ax.set_xlabel(None)
+    ax.tick_params(axis='x', labelsize=35)
+    ax.tick_params(axis='y', labelsize=30)
+    st.pyplot(fig)
+
+with col2:
+    fig, ax = plt.subplots(figsize=(20, 10))
+
+    colors = ["#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+    bystate_df = bystate_df.sort_values(by="customer_count", ascending=False)
+
+    top_5_states = bystate_df.head(8)
+    sns.barplot(
+        y="customer_count",
+        x="customer_state",
+        data=top_5_states.sort_values(by="customer_state", ascending=True),
+        palette=colors,
+        ax=ax
+    )
+    ax.set_title("Number of Customer by State", loc="center", fontsize=50)
+    ax.set_ylabel(None)
+    ax.set_xlabel(None)
+    ax.tick_params(axis='x', labelsize=35)
+    ax.tick_params(axis='y', labelsize=30)
+    st.pyplot(fig)
+
+st.subheader("Best Customer Based on RFM Parameters")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    avg_recency = round(rfm_df.recency.mean(), 1)
+    st.metric("Average Recency (days)", value=avg_recency)
+
+with col2:
+    avg_frequency = round(rfm_df.frequency.mean(), 2)
+    st.metric("Average Frequency", value=avg_frequency)
+
+with col3:
+    avg_frequency = format_currency(
+        rfm_df.monetary.mean(), "AUD", locale='es_CO')
+    st.metric("Average Monetary", value=avg_frequency)
+
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
+colors = ["#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9"]
+
+sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(
+    by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("customer_id", fontsize=30)
+ax[0].set_title("By Recency (days)", loc="center", fontsize=50)
+ax[0].tick_params(axis='y', labelsize=30)
+ax[0].tick_params(axis='x', labelsize=35)
+ax[0].set_xticklabels(ax[0].get_xticklabels(), rotation=45,
+                      horizontalalignment='right', fontsize=12)
+
+
+sns.barplot(y="frequency", x="customer_id", data=rfm_df.sort_values(
+    by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("customer_id", fontsize=30)
+ax[1].set_title("By Frequency", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=30)
+ax[1].tick_params(axis='x', labelsize=35)
+ax[1].set_xticklabels(ax[1].get_xticklabels(), rotation=45,
+                      horizontalalignment='right', fontsize=12)
+
+
+sns.barplot(y="monetary", x="customer_id", data=rfm_df.sort_values(
+    by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
+ax[2].set_ylabel(None)
+ax[2].set_xlabel("customer_id", fontsize=30)
+ax[2].set_title("By Monetary", loc="center", fontsize=50)
+ax[2].tick_params(axis='y', labelsize=30)
+ax[2].tick_params(axis='x', labelsize=35)
+ax[2].set_xticklabels(ax[2].get_xticklabels(), rotation=45,
+                      horizontalalignment='right', fontsize=12)
+
+st.pyplot(fig)
+
+st.caption('Copyright (c) Misno 2023')
